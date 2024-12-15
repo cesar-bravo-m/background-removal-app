8d7b7d9b41754d6359d9c637dce615bc24b59853
César Bravo <cbravo@bcyt.cl>
2024-12-14 22:50:53 -0300

    .

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 import { useState } from "react";
+import '../config.ts';
+
 export default function Home() {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [previewUrl, setPreviewUrl] = useState<string | null>(null);
